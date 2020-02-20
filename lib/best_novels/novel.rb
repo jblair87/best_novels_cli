e08499418959486942b6fad661757f828518e53a
jblair87 <jenna.b.mandel@gmail.com>
2020-02-20 14:04:16 -0500

    update class

--- lib/best_novels/novel.rb
+++ lib/best_novels/novel.rb
@@ -1,5 +1,17 @@
 class BestNovels::Novel
+@@all = []
+attr_accessor :title
 
+def initialize(title)
+  @title = title
+  save
+end
 
-  
+def self.all
+  @@all
+end
+
+def save
+  @@all << self
+end
 end
