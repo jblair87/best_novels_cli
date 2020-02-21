8ca0f75ceb26fbe9e811387c2ad9a201bd10ca37
jblair87 <jenna.b.mandel@gmail.com>
2020-02-21 08:51:29 -0500

    add details of novels

--- lib/best_novels/cli.rb
+++ lib/best_novels/cli.rb
@@ -4,7 +4,10 @@ def call
   get_novels
   list_novels
   get_user_novel
+  what_next
 end
+
+
 
 def get_novels
   @novels = BestNovels::Novel.all
@@ -20,14 +23,28 @@ end
 def get_user_novel
   chosen_novel = gets.strip.to_i
   show_details_for(chosen_novel) if valid_input(chosen_novel, @novels)
+
 end
 
 def valid_input(input, data)
   input.to_i <= data.length && input.to_i < 0
 end
 
+
+
 def show_details_for(chosen_novel)
-  novel = @novels[chosen_novel - 1]
-  puts "Here is the summary for #{novel}"
+  puts novel.title
+  novel.key_info.each {|i| puts "- #{i}"}
 end
+
+def what_next
+   puts "Are you done? Type 'exit' to exit or view an additional novel."
+   @input = gets.strip
+ end
+
+ def goodbye
+   puts "Thanks! Goodbye"
+ end
+
+
 end
--- lib/best_novels/novel.rb
+++ lib/best_novels/novel.rb
@@ -1,9 +1,11 @@
 class BestNovels::Novel
 @@all = []
-attr_accessor :title
+attr_accessor :title, :url, :details
 
-def initialize(title)
+def initialize(title, url)
   @title = title
+  @url = url
+  @details = []
   save
 end
 
@@ -11,6 +13,10 @@ def self.all
   BestNovels::Scraper.scrape_novels if @@all.empty?
   @@all
 end
+
+def get_novel_details
+BestNovels::Scraper.scrape_key_info(self) if @key_info.empty?
+ end
 
 def save
   @@all << self
