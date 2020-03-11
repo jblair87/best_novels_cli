ab2413755e55886eba24f25a3ac121d70f37083a
jblair87 <jenna.b.mandel@gmail.com>
2020-03-11 10:09:27 -0400

    add color to cli

--- lib/best_novels/cli.rb
+++ lib/best_novels/cli.rb
@@ -1,6 +1,6 @@
 class BestNovels::CLI
 def call
-  puts "Welcome to The Guardian's List of 100 Best Novels!"
+  puts "Welcome to The Guardian's List of 100 Best Novels!".colorize(:blue)
   @input = ""
   until @input == "exit"
   get_novels
@@ -18,7 +18,7 @@ end
 
 def list_novels
   puts ''
-  puts 'Choose a novel to read a review from The Guardian.'
+  puts 'Choose a novel to read a review from The Guardian.'.colorize(:blue)
   puts ''
 
   @novels.each.with_index(1) do |novel, index|
@@ -42,23 +42,24 @@ def show_details_for(chosen_novel)
   novel = @novels[chosen_novel - 1]
   novel.get_novel_details
   puts ""
-  puts "The Guardian's Review of: #{novel.title}"
+  puts "The Guardian's Review of ".colorize(:blue) +
+   "#{novel.title}".colorize(:blue).bold
   puts ""
   puts novel.review
   puts ""
-  puts "Read the full review:"
-  puts "#{novel.url}"
+  puts "Read the full review:".colorize(:blue)
+  puts "#{novel.url}".colorize(:blue).underline
 end
 
 def what_next
   puts ""
   puts ""
-  puts "Are you done? Type 'exit' to exit or press any key to view the full list of novels."
+  puts "Are you done? Type 'exit' to exit or press any key to view the full list of novels.".colorize(:blue)
   @input = gets.strip
  end
 
  def goodbye
-   puts "Thanks! Goodbye"
+   puts "Thanks! Goodbye".colorize(:red)
  end
 
 
